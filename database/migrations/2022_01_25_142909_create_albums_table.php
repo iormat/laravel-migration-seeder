c39df0d0ca1f00a52d22f24a8a163d1aae57e1d3
iormat <iorio436@outlook.it>
2022-01-25 15:35:04 +0100

    add songs, albums, artists tables

---
+++ database/migrations/2022_01_25_142909_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('artist_id');
+            $table->foreignId('song_id');
+            $table->string('name', 30);
+            $table->date('date_of_release');
+            $table->string('genre', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
